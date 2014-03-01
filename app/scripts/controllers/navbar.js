cec0c8ccc692904ca18b409294e0cdfdd0a27c5b
Stefan Valentin <stefan@vubeology.com>
2014-03-01 01:13:43 -0600

    Init commit with generator-angular-fullstack.

---
+++ app/scripts/controllers/navbar.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('tyskApp')
+  .controller('NavbarCtrl', function ($scope, $location) {
+    $scope.menu = [{
+      'title': 'Home',
+      'link': '/'
+    }];
+    
+    $scope.isActive = function(route) {
+      return route === $location.path();
+    };
+  });
